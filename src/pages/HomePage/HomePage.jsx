c0349f7022d4bb44e8ace37115bbcd6cc53b0ab3
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2023-09-26 00:35:55 +0300

    test

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -8,7 +8,7 @@ export const HomePage = () => {
                 <p>
                     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto,
                     laboriosam placeat incidunt rem illum animi nemo quibusdam quia
-                    voluptatum voluptate.
+                    voluptatum voluptate. ghyjtyjctf
                 </p>
         </main>
     )
