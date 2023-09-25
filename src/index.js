af681ee8313e8680b22007389e7018e3f877a22e
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2023-09-26 00:22:19 +0300

    test

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import {BrowserRouter} from "react-router-dom"
 import { App } from './components/App';
 import './index.css';
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <BrowserRouter >
