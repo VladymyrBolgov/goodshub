a5e8ee8fddb0a880771bee9de443fbb636ee0ce2
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2023-09-26 22:33:10 +0300

    br

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -6,6 +6,7 @@ export const HomePage = () => {
             <p>эта страница под кнопкой Home временная, эта кнопка Home уберется,
                 когда начнем делать хедер. Эта главная домашняя страница,
                 возврат на нее будет через логотип GoodsHub</p>
+            <br></br>
             <img src="https://via.placeholder.com/960x240" alt="" />
             <h1>Promotional offers</h1>
             <br></br>
