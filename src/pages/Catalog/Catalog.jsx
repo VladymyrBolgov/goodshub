99876f7462de54095f8ade69e904b633f8dc8e7b
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2023-09-26 22:17:29 +0300

    header

--- src/pages/Catalog/Catalog.jsx
+++ src/pages/Catalog/Catalog.jsx
@@ -2,6 +2,9 @@ export const Catalog = () => {
     return (
       <main>
         <h1>Catalog</h1>
+        <br></br>
+        <p>это каталог товара, кнопка есть у всех пользователей</p>
+        <br></br>
         <ol>Стиральные машины
           <li>Лж</li>
           <li>Самсунг</li>
