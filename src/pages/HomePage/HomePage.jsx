9dee0328d9a9de344e501a49d01f4cb000753412
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2023-09-26 17:12:33 +0300

    home

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,8 +3,9 @@ import React from "react";
 export const HomePage = () => {
     return (
         <main>
+            <img src="https://via.placeholder.com/960x240" alt="" />
             <h1>Promotional offers</h1>
-                <img src="https://via.placeholder.com/960x240" alt="" />
+            <br></br>
                 <ol>Стиральные машины
                     <li>Лж</li>
                     <li>Самсунг</li>
