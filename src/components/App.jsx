ee550a09ac61b279865358921152812b45f4146b
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2023-09-26 22:58:06 +0300

    GoodsHub

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 
 import { Catalog } from "../pages/Catalog/Catalog";
-import { HomePage } from "../pages/HomePage/HomePage";
+// import { HomePage } from "../pages/HomePage/HomePage";
+import { GoodsHub} from "../pages/GoodsHub/GoodsHub";
 import { LoginPage } from "../pages/LoginPage/LoginPage";
 import { RegisterPage } from "../pages/RegisterPage/RegisterPage"
 import { Messages } from "../pages/Messages/Messages";
@@ -23,7 +24,7 @@ export const App = () => {
         </Logo>
         <nav>
           <Link to="/catalog">Catalog</Link>
-          <Link to="/" end>Home</Link>
+          {/* <Link to="/" end>Home</Link> */}
           <Link to="/login">Login</Link>
           <Link to="/messages">Messages</Link>
           <Link to="/favorites">Favorites</Link>
@@ -32,7 +33,8 @@ export const App = () => {
       </Header>
 
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/" element={<HomePage />} /> */}
+        <Route path="/" element={<GoodsHub />} />
         <Route path="/catalog" element={<Catalog />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
@@ -44,3 +46,52 @@ export const App = () => {
     </Container>
   );
 };
+
+
+
+// import { Routes, Route } from "react-router-dom";
+
+// import { Catalog } from "../pages/Catalog/Catalog";
+// import { HomePage } from "../pages/HomePage/HomePage";
+// import { LoginPage } from "../pages/LoginPage/LoginPage";
+// import { RegisterPage } from "../pages/RegisterPage/RegisterPage"
+// import { Messages } from "../pages/Messages/Messages";
+// import { Favorites } from "../pages/Favorites/Favorites";
+// import { Basket } from "../pages/Basket.jsx/Basket";
+// import { NotFoundPage } from "../pages/NotFoundPage/NotFoundPage";
+
+// import { Container, Header, Logo, Link } from "./App.styled";
+
+// export const App = () => {
+//   return (
+//     <Container>
+//        <Header>
+//         <Logo>
+//           <span role="img" aria-label="computer icon">
+//             💻
+//           </span>{" "}
+//           <Link to="/" end>GoodsHub</Link>
+//         </Logo>
+//         <nav>
+//           <Link to="/catalog">Catalog</Link>
+//           <Link to="/" end>Home</Link>
+//           <Link to="/login">Login</Link>
+//           <Link to="/messages">Messages</Link>
+//           <Link to="/favorites">Favorites</Link>
+//           <Link to="/basket">Basket</Link>
+//         </nav>
+//       </Header>
+
+//       <Routes>
+//         <Route path="/" element={<HomePage />} />
+//         <Route path="/catalog" element={<Catalog />} />
+//         <Route path="/register" element={<RegisterPage />} />
+//         <Route path="/login" element={<LoginPage />} />
+//         <Route path="/messages" element={<Messages />} />
+//         <Route path="/favorites" element={<Favorites />} />
+//         <Route path="/basket" element={<Basket />} />
+//         <Route path="*" element={<NotFoundPage/>} /> 
+//       </Routes>
+//     </Container>
+//   );
+// };
---
+++ src/pages/GoodsHub/GoodsHub.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+export const GoodsHub = () => {
+    return (
+        <main>
+           
+            <br></br>
+            <img src="https://via.placeholder.com/960x240" alt="" />
+            <h1>Promotional offers</h1>
+            <br></br>
+                <ol>Стиральные машины
+                    <li>Лж</li>
+                    <li>Самсунг</li>
+                    <li>Индезит</li>
+                    <li>Аристон</li>
+                </ol>
+                    <br></br>
+                <ol>Стиральные машины
+                    <li>Лж</li>
+                    <li>Самсунг</li>
+                    <li>Индезит</li>
+                    <li>Аристон</li>
+                </ol>
+                    <br></br>
+                 <ol>Стиральные машины
+                    <li>Лж</li>
+                    <li>Самсунг</li>
+                    <li>Индезит</li>
+                    <li>Аристон</li>
+                </ol>
+                    <br></br>
+                <ol>Стиральные машины
+                    <li>Лж</li>
+                    <li>Самсунг</li>
+                    <li>Индезит</li>
+                    <li>Аристон</li>
+                </ol>
+        </main>
+    )
+}
