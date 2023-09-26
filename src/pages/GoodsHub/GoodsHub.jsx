36c8e270bdf4585a2221619486f3ca16eedb0b6b
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2023-09-26 23:16:19 +0300

    footer

--- src/pages/GoodsHub/GoodsHub.jsx
+++ src/pages/GoodsHub/GoodsHub.jsx
@@ -6,35 +6,53 @@ export const GoodsHub = () => {
            
             <br></br>
             <img src="https://via.placeholder.com/960x240" alt="" />
-            <h1>Promotional offers</h1>
+            <h2>Promotional offers</h2>
             <br></br>
                 <ol>Стиральные машины
                     <li>Лж</li>
                     <li>Самсунг</li>
                     <li>Индезит</li>
                     <li>Аристон</li>
+                    <li>Бош</li>
+                    <li>Сименс</li>
                 </ol>
+                    <br></br>
+            <h2>Hot new products</h2>
                     <br></br>
                 <ol>Стиральные машины
                     <li>Лж</li>
                     <li>Самсунг</li>
                     <li>Индезит</li>
                     <li>Аристон</li>
+                    <li>Бош</li>
+                    <li>Сименс</li>
                 </ol>
+                    <br></br>
+            <h2>More products to choose from</h2>
                     <br></br>
                  <ol>Стиральные машины
                     <li>Лж</li>
                     <li>Самсунг</li>
                     <li>Индезит</li>
                     <li>Аристон</li>
+                    <li>Бош</li>
+                    <li>Сименс</li>
                 </ol>
-                    <br></br>
-                <ol>Стиральные машины
-                    <li>Лж</li>
-                    <li>Самсунг</li>
-                    <li>Индезит</li>
-                    <li>Аристон</li>
-                </ol>
+            <br></br>
+            <br></br>
+            <br></br>
+            <footer>
+                <h2>Logo</h2>
+                <br></br>
+                <p>Privacy Policy</p>
+                <p>Help&FAQ</p>
+                <p>Payment and delivery</p>
+                <p>Return Policy</p>
+                <p>How to sell and buy?</p>
+                <br></br>
+                <p>© 2023 GoodsHub. All rights reserved.</p>
+            </footer>
+             
         </main>
     )
 }
