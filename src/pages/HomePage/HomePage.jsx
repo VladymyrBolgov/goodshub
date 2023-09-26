544f2c3feded8f65a351080980a8c7b84e86cc9b
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2023-09-26 22:27:41 +0300

    header

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,6 +3,9 @@ import React from "react";
 export const HomePage = () => {
     return (
         <main>
+            <p>эта страница временная, эта кнопка Home уберется,
+                когда начнем делать хедер. Эта главная домашняя страница,
+                возврат на нее будет через логотип</p>
             <img src="https://via.placeholder.com/960x240" alt="" />
             <h1>Promotional offers</h1>
             <br></br>
