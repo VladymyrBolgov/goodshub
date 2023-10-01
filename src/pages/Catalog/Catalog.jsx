3f12f745a341cea573d948fef187295998d8b7b1
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2023-10-01 23:32:30 +0300

    правки

--- src/pages/Catalog/Catalog.jsx
+++ src/pages/Catalog/Catalog.jsx
@@ -1,27 +1,3 @@
-// export const Catalog = () => {
-//     return (
-//       <main>
-//         <h1>Catalog</h1>
-//         <br></br>
-//         <p>это каталог товара, кнопка есть у всех пользователей</p>
-//         <br></br>
-//         <ol>Стиральные машины
-//           <li>Лж</li>
-//           <li>Самсунг</li>
-//           <li>Индезит</li>
-//           <li>Аристон</li>
-//         </ol>
-//         <br></br>
-//         <ol>Стиральные машины
-//           <li>Лж</li>
-//           <li>Самсунг</li>
-//           <li>Индезит</li>
-//           <li>Аристон</li>
-//         </ol>
-//       </main>
-//     );
-// };
-  
 import { ProductList } from "../../components/ProductList/ProductList";
 import { getProducts } from "../../fakeAPI";
 
