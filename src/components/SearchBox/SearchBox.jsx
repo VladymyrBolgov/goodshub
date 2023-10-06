2f842ef5895a99795e2ecb4a741d4b697439f308
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2023-10-06 19:47:40 +0300

    search

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,15 +1,20 @@
 import { Wrapper, Input, Icon, Button } from "./SearchBox.styled";
+import { useSearchParams } from "react-router-dom";
 
-export const SearchBox = ({ value, onChange }) => {
+export const SearchBox = () => {
+    
+    const [searchParams, setSearchParams] = useSearchParams();
+    const productId = searchParams.get('productId');
+   
   return (
       <Wrapper>
       <Icon />
       <Input
         type="text"
-        value={value}
-              onChange={(e) => onChange(e.target.value)}    
+        value={productId}
+        onChange= {(e) => setSearchParams({productId: e.target.value})}    
           />
-        <Button>Search</Button>
+        <Button onClick={() => setSearchParams({productId: 'productId'})}>Search</Button>
     </Wrapper>
   );
 };
