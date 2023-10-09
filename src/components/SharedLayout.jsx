e1edefe3ea0abc6891a93da5d62d4e752ec7d19c
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2023-10-10 00:06:11 +0300

    icon

--- src/components/SharedLayout.jsx
+++ src/components/SharedLayout.jsx
@@ -2,6 +2,10 @@ import { useSearchParams } from "react-router-dom";
 import { Outlet } from 'react-router-dom';
 import { Container, Header, Logo, Link } from './SharedLayout.styled';
 import { SearchBox } from './SearchBox/SearchBox';
+import { MdOutlineShoppingBasket } from 'react-icons/md';
+import { GrFavorite } from 'react-icons/gr';
+import { BsChatLeft } from 'react-icons/bs';
+import { BsPerson } from 'react-icons/bs';
 
 export const SharedLayout = () => {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -20,10 +24,10 @@ export const SharedLayout = () => {
           <Link to="/catalog">Catalog</Link>
           <SearchBox value={productName} onChange={updateQueryString} />
           <nav>
-            <Link to="/login">Login</Link>
-            <Link to="/messages">Messages</Link>
-            <Link to="/favorites">Favorites</Link>
-            <Link to="/basket">Basket</Link>
+            <Link to="/login"><BsPerson /></Link>
+            <Link to="/messages"><BsChatLeft /></Link>
+            <Link to="/favorites"><GrFavorite /></Link>
+            <Link to="/basket"><MdOutlineShoppingBasket /></Link>
           </nav>
         </Header>
         <Outlet />
